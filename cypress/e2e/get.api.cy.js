ba4479ba24358ecb13c2f849a9e5f8c01bdbdbc2
Ravi Silva <ravifelipee@gmail.com>
2024-06-04 21:14:21 -0300

    method GET validations

---
+++ cypress/e2e/get.api.cy.js
@@ -0,0 +1,34 @@
+/// <reference types="cypress"/>
+
+describe('List devices', () => {
+
+    it('List SINGLE OBJECT', () => {
+        const device__id = '7'
+
+        cy.request({
+            method: 'GET',
+            url: `https://api.restful-api.dev/objects/${device__id}`,
+            failOnStatusCode: false
+        }).as('getDeviceResult')
+
+
+        //validations
+        cy.get('@getDeviceResult')
+            .then((response) => {
+                //console.log(response)
+
+                expect(response.status).equal(200)
+                expect(response.body.id).equal(device__id)
+                expect(response.body.name).equal('Apple MacBook Pro 16')
+                expect(response.body).not.empty
+                expect(response.body.data).not.empty
+                expect(response.body.data.year).not.empty
+                expect(response.body.data.year).equal(2019)
+                expect(response.body.data.price).not.empty
+                expect(response.body.data.price).equal(1849.99)
+                expect(response.body.data['CPU model']).not.empty
+                expect(response.body.data['Hard disk size']).not.empty
+
+            })
+    })
+})
---
+++ cypress/e2e/post.api.cy.js
@@ -0,0 +1,8 @@
+/// <reference types="cypress"/>
+
+describe('', () => {
+
+    it('', () => {
+
+    })
+})
