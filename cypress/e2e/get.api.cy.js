213cdccb586519a6f396396ce81391ca4178b8b9
Ravi Silva <ravifelipee@gmail.com>
2025-01-02 08:31:27 -0300

    Implementation of the POST tests

--- cypress/e2e/get.api.cy.js
+++ cypress/e2e/get.api.cy.js
@@ -15,19 +15,24 @@ describe('List devices', () => {
         //validations
         cy.get('@getDeviceResult')
             .then((response) => {
-                //console.log(response)
 
-                expect(response.status).equal(200)
-                expect(response.body.id).equal(device__id)
-                expect(response.body.name).equal('Apple MacBook Pro 16')
-                expect(response.body).not.empty
-                expect(response.body.data).not.empty
-                expect(response.body.data.year).not.empty
-                expect(response.body.data.year).equal(2019)
-                expect(response.body.data.price).not.empty
-                expect(response.body.data.price).equal(1849.99)
-                expect(response.body.data['CPU model']).not.empty
-                expect(response.body.data['Hard disk size']).not.empty
+                expect(response.status).equal(200);
+                expect(response.body).not.empty;
+
+                expect(response.body.id).equal(device__id);
+                expect(response.body.name).equal('Apple MacBook Pro 16');
+                expect(response.body.data).not.empty;
+
+                expect(response.body.data.year).not.string;
+                expect(response.body.data.year).equal(2019);
+
+                //expect(response.body.data.price).not.empty
+                expect(response.body.data.price).to.exist;
+                expect(response.body.data.price).to.be.a('number');
+                expect(response.body.data.price).equal(1849.99);
+
+                expect(response.body.data['CPU model']).not.empty;
+                expect(response.body.data['Hard disk size']).not.empty;
 
             })
     })
--- cypress/e2e/post.api.cy.js
+++ cypress/e2e/post.api.cy.js
@@ -1,8 +1,44 @@
 /// <reference types="cypress"/>
 
-describe('', () => {
+describe('Register device', () => {
 
-    it('', () => {
+    it('Register device', () => {
+        const currentDate = new Date().toISOString().slice(0, 10);
 
+        const body = {
+            "name": "Ravi Laptop",
+            "data": {
+                "year": 2024,
+                "price": 1849.99,
+                "CPU model": "Intel Core i5",
+                "Hard disk size": "1 TB",
+                "Owner": "Ravi Silva LTDA"
+            }
+        }
+
+        cy.request({
+            method: 'POST',
+            url: `https://api.restful-api.dev/objects`,
+            failOnStatusCode: false,
+            body: body
+        }).as('postDeviceResult')
+
+        //validations
+        //pegar o Response e fazer as validacoes -> 
+        cy.get('@postDeviceResult')
+            .then((response) => {
+                expect(response.status).equal(200);
+                expect(response.body.id).not.to.be.null;
+                expect(response.body.id).not.to.be.empty;
+
+                expect(response.body.createdAt).not.to.be.null;
+                expect(response.body.createdAt).to.not.be.empty;
+
+                expect(response.body.createdAt.slice(0, 10)).equal(currentDate);
+                expect(response.body.name).equal(body.name);
+
+                expect(response.body.data.year).equal(body.data.year);
+                expect(response.body.data.price).equal(body.data.price);
+            })
     })
 })
